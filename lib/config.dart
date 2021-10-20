741b80b765e47819d56eaeeadd410ab3022f9ccc
Julien LE BREN <julien@lebren.fr>
2021-10-20 17:02:10 +0200

    .gitignore fix

--- lib/config.dart
+++
@@ -1,9 +0,0 @@
-const isbnApiKey = "46315_6af17db87d912cb5debd025db35ddedf";
-
-const purchasesApiKey = 'tzYWZCavBxiDHDfMoteeDtcDRNrSrFfa';
-const entitlementId = 'premium';
-
-const privacyPolicyURL = "https://boobook.school/privacy-policy";
-
-const googlePlayURL = "https://play.google.com/store/account/subscriptions";
-const appStoreURL = "https://apps.apple.com/account/subscriptions";
